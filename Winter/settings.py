af6ed8a530f1a6fe741389fd1b80e11ef7edb7d9
theweblover007 <theweblover007@gmail.com>
2019-01-06 17:47:45 +0530

    Changed the secret key.

--- Winter/settings.py
+++ Winter/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6klc*!xlig-0sf#t*=7uqe3g3(5rm@77c10^gjg-q6r5on%#cq'
+with open('etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
